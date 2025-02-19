6178325a829a20bb6f3550474719173464e8ee12
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2025-02-18 21:36:27 -0300

    Feat(Tailwind.Config):Add DateColor

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -10,6 +10,7 @@ const config: Config = {
   theme: {
   	extend: {
   		colors: {
+			dateColor: '#6941C6',
   			background: 'hsl(var(--background))',
   			foreground: 'hsl(var(--foreground))',
   			card: {
