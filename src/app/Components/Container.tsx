ab32759a62f79980f54425f8d35404022a2d55fb
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2025-02-21 12:01:38 -0300

    refactor(container.tsx): add responsiveness to component

--- src/app/Components/Container.tsx
+++ src/app/Components/Container.tsx
@@ -5,5 +5,5 @@ interface ContainerProps {
   }
   
   export const Container: React.FC<ContainerProps> = ({ children }) => {
-    return <div className="py-3 px-6">{children}</div>;
-  };
+    return <div className="py-3 px-6 md:px-20 lg:px-32">{children}</div>;
+  }
