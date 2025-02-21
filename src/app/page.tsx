fe8e73a64e25687492c6058e124a19479f7b7bad
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2025-02-21 12:04:50 -0300

    feat(page.tsx): add post component, container component, pagination menu component and footer component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,24 @@
+import { Container } from "./Components/Container";
+import {Footer} from "./Components/Footer";
 import { Header } from "./Components/Header";
 import { Navbar } from "./Components/Navbar";
+import { PaginationMenu } from "./Components/Pagination";
+import { Post } from "./Components/Post";
+import image from './assets/Image.png'
 
 export default function Home() {
   return (
-    <>
+   <>
     <Navbar/>
-    <Header title="Recent blog posts"/>
-    </>
+    <Container>      
+      <Header title="Recent blog posts"/>
+      <Post date={new Date()} imgsrc={image} key='1' title="title1" content="testing content with new feature called lorem ipsum testing content with new feature called lorem ipsum" tags={['Node','Typescript']}/>
+      <Post date={new Date()} imgsrc={image} key='2' title="title2" content="testing content with new feature called lorem ipsum testing content with new feature called lorem ipsum" tags={['Node','Typescript']}/>
+      <Post date={new Date()} imgsrc={image} key='3' title="title3" content="testing content with new feature called lorem ipsum testing content with new feature called lorem ipsum" tags={['Node','Typescript']}/>
+      <PaginationMenu/>
+      <Footer/>
+    </Container>
+   </>
   );
 }
+ 
