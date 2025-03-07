883a22e7b79b1743b0a85a1a4b981e3393694247
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2025-03-07 14:59:51 -0300

    refactor(page.tsx):add removes post and add feed component

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,39 @@
 import { Container } from "./Components/Container";
+import { Feed } from "./Components/Feed";
 import {Footer} from "./Components/Footer";
 import { Header } from "./Components/Header";
 import { Navbar } from "./Components/Navbar";
 import { PaginationMenu } from "./Components/Pagination";
-import { Post } from "./Components/Post";
 import image from './assets/Image.png'
+
+
+const mockdata = [
+  {
+    id: '1',
+    date: new Date(),
+    imgsrc: image,
+    title: 'Title 1',
+    content: 'Testing content with new feature called lorem ipsum testing content with new feature called lorem ipsum',
+    tags: ['Node', 'Typescript'],
+  },
+  {
+    id: '2',
+    date: new Date(),
+    imgsrc: image,
+    title: 'Title 2',
+    content: 'Testing content with new feature called lorem ipsum testing content with new feature called lorem ipsum',
+    tags: ['React', 'JavaScript'],
+  },
+  {
+    id: '3',
+    date: new Date(),
+    imgsrc: image,
+    title: 'Title 3',
+    content: 'Testing content with new feature called lorem ipsum testing content with new feature called lorem ipsum',
+    tags: ['CSS', 'HTML'],
+  },
+];
+
 
 export default function Home() {
   return (
@@ -12,9 +41,7 @@ export default function Home() {
     <Navbar/>
     <Container>      
       <Header title="Recent blog posts"/>
-      <Post date={new Date()} imgsrc={image} key='1' title="title1" content="testing content with new feature called lorem ipsum testing content with new feature called lorem ipsum" tags={['Node','Typescript']}/>
-      <Post date={new Date()} imgsrc={image} key='2' title="title2" content="testing content with new feature called lorem ipsum testing content with new feature called lorem ipsum" tags={['Node','Typescript']}/>
-      <Post date={new Date()} imgsrc={image} key='3' title="title3" content="testing content with new feature called lorem ipsum testing content with new feature called lorem ipsum" tags={['Node','Typescript']}/>
+        <Feed data={mockdata}/>
       <PaginationMenu/>
       <Footer/>
     </Container>
