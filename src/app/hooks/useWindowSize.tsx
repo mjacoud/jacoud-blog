4ba07a6c61abc1bb7167a71886dbf05e7317d852
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2025-03-07 20:36:53 -0300

    feat(useWindowsize.tsx): add windowsize hook

---
+++ src/app/hooks/useWindowSize.tsx
@@ -0,0 +1,34 @@
+'use client'
+
+import { useState, useEffect } from "react";
+
+export const useWindowSize = () => {
+  const [windowSize, setWindowSize] = useState({
+    width: 0,
+    height: 0,
+  });
+
+  useEffect(() => {
+    // Only execute this code on the client side
+    if (typeof window !== "undefined") {
+      const handleResize = () => {
+        setWindowSize({
+          width: window.innerWidth,
+          height: window.innerHeight,
+        });
+      };
+
+      // Set initial size
+      handleResize();
+
+      window.addEventListener("resize", handleResize);
+
+      // Cleanup event listener on component unmount
+      return () => {
+        window.removeEventListener("resize", handleResize);
+      };
+    }
+  }, []);
+
+  return windowSize;
+};
