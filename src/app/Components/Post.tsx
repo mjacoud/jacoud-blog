c12b341316184107a9a0e82fae0d8df17f38aa48
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2025-03-07 20:46:52 -0300

    feat(post.tsx):add new type of post layout

--- src/app/Components/Post.tsx
+++ src/app/Components/Post.tsx
@@ -8,9 +8,10 @@ interface PostProps {
     imgsrc: string | StaticImageData; // StaticImageData é um tipo temporário, que será substituído por um link para a imagem
     date: Date
     tags:string[]
+    type:string
     }
 
-export const Post:React.FC<PostProps> = ({title,content,date,imgsrc,tags}:PostProps) => {
+export const Post:React.FC<PostProps> = ({title,content,date,imgsrc,tags,type}:PostProps) => {
   
   const formatedDate = (date:Date) => {
     const weekDays = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thursday', 'Friday', 'Saturday']
@@ -33,23 +34,51 @@ export const Post:React.FC<PostProps> = ({title,content,date,imgsrc,tags}:PostPr
 };
 
   return (
-    <div>
+    <>
+     {type == "main" ?
+     <div>
         <Link href={`/${title}`}>
-          <Image src={imgsrc} alt="post image" width={500} height={300} className="pb-10"/>
-                <div className="font-inter font-semibold text-strongPurple text-sm ">{formatedDate(date)}</div>
+          <Image src={imgsrc} alt="post image" width={0} height={0} sizes="100vw" className="pb-10 w-full h-auto md:h-[20rem]"/>
+                <div className="font-inter font-semibold text-strongPurple text-sm md:text-base ">{formatedDate(date)}</div>
                 <div className="flex justify-between align-bottom">
                 <h2 className="py-4 text-3xl font-inter font-semibold dark:text-weakPurple">{title}</h2>
           <Image src={arrowUp} alt="arrow-up" width={40} height={68}/>
                 </div>
-                <p className="pb-6 font-inter text-gray-400">{content}</p>
+                <p className="pb-6 font-inter text-gray-400  md:text-lg">{content}</p>
         </Link>
                 <div className="flex flex-wrap gap-2 pb-10">
         {tags.map(tag=>
-        <span key={tag} className={`px-2 py-1 rounded-full text-sm font-medium ${tagColors[tag] || 'bg-gray-200 text-gray-600'}`}>
+        <span key={tag} className={`px-2 py-1 rounded-full text-sm font-medium md:text-base ${tagColors[tag] || 'bg-gray-200 text-gray-600'}`}>
         {tag}
     </span>
         )}
         </div>
     </div>
+  : 
+  <div className="flex">
+    <div>
+      <Link href={`/${title}`}>
+            <Image src={imgsrc} alt="post image" width={0} height={0} sizes="100vw" className="pb-10 w-full h-auto md:h-[19rem] md:w-[50rem] xl:h-[20rem]"/>
+          </Link>
+    </div>
+    <div className="pl-20 xl:pl-10">
+                  <div className="font-inter font-semibold text-strongPurple text-sm md:text-base ">{formatedDate(date)}</div>
+                  <div className="flex justify-between align-bottom">
+                  <h2 className="py-4 text-3xl font-inter font-semibold dark:text-weakPurple">{title}</h2>
+            <Image src={arrowUp} alt="arrow-up" width={40} height={68}/>
+                  </div>
+                  <p className="pb-6 font-inter text-gray-400  md:text-lg">{content}</p>
+                  <div className="flex flex-wrap gap-2 pb-10">
+          {tags.map(tag=>
+          <span key={tag} className={`px-2 py-1 rounded-full text-sm font-medium md:text-base ${tagColors[tag] || 'bg-gray-200 text-gray-600'}`}>
+          {tag}
+      </span>
+          )}     
+        </div>
+    </div>
+  
+  </div>
+}
+  </>
   )
 }
