6668d559da4d1d5d710a57ef846c4af0a824bb1f
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2025-03-07 21:40:46 -0300

    refactor(footer.tsx):add responsiviness to the footer

--- src/app/Components/Footer.tsx
+++ src/app/Components/Footer.tsx
@@ -3,19 +3,21 @@ import React from 'react';
 
 export const Footer: React.FC = () => {
     return (
-        <footer className='pb-6'>
-            <div className='border-t-2 border-gray-200 mt-6 flex flex-col items-center'>
+        <>
+        <footer className='pb-6 md:py-8'>
+            <div className='border-t-2 border-gray-200 mt-6 flex flex-col items-center md:items-start'>
                 <nav>
-                    <ul className="flex flex-col items-center gap-2 py-8">
-                        <li className='font-semibold text-xl'><Link href="/about">LinkedIn</Link></li>
-                        <li className='font-semibold text-xl'><Link href="/contact">Email</Link></li>
-                        <li className='font-semibold text-xl'><Link href="/privacy">Github</Link></li>
-                        <li className='font-semibold text-xl'><Link href="/privacy">Rss Feed</Link></li>
+                    <ul className="flex flex-col items-center gap-6 py-8 md:flex-row  md:gap-12 md:py-12 lg:gap-20 ">
+                        <li className='font-semibold text-2xl'><Link href="/about">LinkedIn</Link></li>
+                        <li className='font-semibold text-2xl'><Link href="/contact">Email</Link></li>
+                        <li className='font-semibold text-2xl'><Link href="/privacy">Github</Link></li>
+                        <li className='font-semibold text-2xl'><Link href="/privacy">RSS Feed</Link></li>
+                    <p className='font-semibold text-xl'>&copy; {new Date().getFullYear()} </p>
                     </ul>
                 </nav>
-                <p className='font-semibold text-xl'>&copy; {new Date().getFullYear()} </p>
             </div>
         </footer>
+        </>
     );
 };
 
