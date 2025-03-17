cf6b7d42a983692d9c0320da33c1ed255e0ce90a
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2025-03-17 20:18:15 -0300

    feat(header.tsx):add type

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -1,9 +1,10 @@
 interface headerProps{
     title: string;
+    type:string;
 }
 
-export const Header = ({title}: headerProps) => {
+export const Header = ({title,type}: headerProps) => {
   return (
-    <div className="pt-24 pb-8 text-2xl font-bold xl:text-5xl xl:pt-36">{title}</div>
+    <div className={`${type== 'primary' ? 'pt-24': 'md:pt-24'} pb-8 text-2xl font-bold xl:text-5xl xl:pt-36`}>{title}</div>
   )
 }
