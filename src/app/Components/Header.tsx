5864bc4df45442ed0ee8e99ed00e1f64bb39b154
Matheus Jacoud <63015457+mjacoud@users.noreply.github.com>
2025-03-17 20:19:50 -0300

    tweak(Header.tsx): change font size

--- src/app/Components/Header.tsx
+++ src/app/Components/Header.tsx
@@ -5,6 +5,6 @@ interface headerProps{
 
 export const Header = ({title,type}: headerProps) => {
   return (
-    <div className={`${type== 'primary' ? 'pt-24': 'md:pt-24'} pb-8 text-2xl font-bold xl:text-5xl xl:pt-36`}>{title}</div>
+    <div className={`${type== 'primary' ? 'pt-24': 'md:pt-24'} pb-8 text-3xl font-bold xl:text-5xl xl:pt-36`}>{title}</div>
   )
 }
